24a71ae189d2012f49db52eefece4b5afef0305f
Julia Fehr <09jwithers@gmail.com>
2020-01-09 22:22:39 -0500

    now able to search multiple states, adjusted css

--- code.js
+++ code.js
@@ -4,26 +4,22 @@ const baseURL='https://developer.nps.gov/api/v1/parks?';
 const geoSource = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 // setup for the rendering. alters some existing html as well
-function renderResults(data,maxresults,stateSearch){
-    console.log(data)
-    // clear the lists every time a new entry is made
-    $('#returned-results').empty();
-
-    // show how many results we should expect to see
-    $('.results').html(`Results: ${data.length}`)
-
+function renderResults(data,maxresults,stateSearch,id){
     // must designate iteration length by input data
     if(data.length <= maxresults){
         for(let i=0;i<data.length;i++){
-            renderPark(data[i],stateSearch)            
+            renderPark(data[i],stateSearch,id)            
         };
+        // show how many results we should expect to see
+        $('#results'+id).html(`${data.length}`)
     }
     else if (data.length > maxresults){
         for(let i=0;i<maxresults;i++){
-            renderPark(data[i],stateSearch)             
+            renderPark(data[i],stateSearch,id)             
         };
+        // show how many results we should expect to see
+        $('#results'+id).html(`${maxresults}`)
     }
-
     // if no data is available show a response
     if(data.length === 0){
         $('#other').text(`No Results Found`)
@@ -34,12 +30,12 @@ function renderResults(data,maxresults,stateSearch){
 }
 
 // render the html code
-function renderPark(parkInfo, stateSearch){
+function renderPark(parkInfo, stateSearch,id){
     let parkHTML = ''
     getAddresses(parkInfo.latLong)
     .then(add => {
         parkHTML = generateParkItemHTML(parkInfo,add, stateSearch)
-        $('#returned-results').append(parkHTML)})    
+        $('#returned-results'+id).append(parkHTML)})    
 }
 
 // generate the html code for the li's
@@ -48,7 +44,7 @@ function generateParkItemHTML(parkInfo,add,stateSearch){
     return `<li class="needClass">
     <p><strong>${parkInfo.fullName}</strong>(<a href= ${parkInfo.url} target="_blank">Link</a>)</p>
     <p>State searched: ${stateSearch}</p>
-    <p><em>Description</em>: ${parkInfo.description}</p><br>
+    <p><em>Description</em>: ${parkInfo.description}</p>
     <p class="address"><em><em>Address: ${add}</em></p>
     </li>`
 }
@@ -82,7 +78,7 @@ function mapParams(params){
 }
 
 // fetch the data
-function getParks(stateSearch, maxresults){
+function getParks(stateSearch, maxresults,i){
     console.log(stateSearch)
     const params = {
         stateCode: stateSearch,
@@ -102,7 +98,7 @@ function getParks(stateSearch, maxresults){
         return response.json()
         }
         throw new Error (response.Status)})
-    .then(responseJson=> renderResults(responseJson.data, maxresults,stateSearch))
+    .then(responseJson=> renderResults(responseJson.data, maxresults,stateSearch,i))
     .catch(err=>{
         $('#error').text(`Something went wrong: ${err.message}`);
         console.log(err.message)
@@ -114,6 +110,8 @@ function getParks(stateSearch, maxresults){
 function watchForm(){
     $('form').submit(e=>{
         e.preventDefault();
+        // clear the lists every time a new entry is made
+        $('#results').empty();
         const maxresults=$('#max-hits').val();
         let val = [];
         $("input[name='state']:checked").each(function(i){
@@ -121,65 +119,17 @@ function watchForm(){
         });
         console.log(val)
         for (let i=0;i<val.length;i++){
-            getParks(val[i],maxresults)
+            $('#results').append(
+                `<div id="${i}">
+                <h2>Results for ${val[i]}:<p id="results${i}"></p></h2>
+                     <ul id="returned-results${i}">
+                         
+                     </ul>
+                </div>`)
+            getParks(val[i],maxresults,i)
+            console.log(val)
         }
-    })
+    })          
 }
 
-// Try to make a nice drop down menu: 
-// sample html code: 
-{/* <select name="search-state" multiple="" id="search">
-<div class="" value="">State</option>
-<div class="item" value="AL">Alabama</div>
-<div class="item" value="AK">Alaska</div>
-<div class="item" value="AZ">Arizona</div>
-<div class="item" value="AR">Arkansas</div>
-<div class="item" value="CA">California</div>
-<div class="item" value="CO">Colorado</div>
-<div class="item" value="CT">Connecticut</div>
-<div class="item" value="DE">Delaware</div>
-<div class="item" value="DC">District Of Columbia</div>
-<div class="item" value="FL">Florida</div>
-<div class="item" value="GA">Georgia</div>
-<div class="item" value="HI">Hawaii</div>
-<div class="item" value="ID">Idaho</div>
-<div class="item" value="IL">Illinois</div>
-<div class="item" value="IN">Indiana</div>
-<div class="item" value="IA">Iowa</div>
-<div class="item" value="KS">Kansas</div>
-<div class="item" value="KY">Kentucky</div>
-<div class="item" value="LA">Louisiana</div>
-<div class="item" value="ME">Maine</div>
-<div class="item" value="MD">Maryland</div>
-<div class="item" value="MA">Massachusetts</div>
-<div class="item" value="MI">Michigan</div>
-<div class="item" value="MN">Minnesota</div>
-<div class="item" value="MS">Mississippi</div>
-<div class="item" value="MO">Missouri</div>
-<div class="item" value="MT">Montana</div>
-<div class="item" value="NE">Nebraska</div>
-<div class="item" value="NV">Nevada</div>
-<div class="item" value="NH">New Hampshire</div>
-<div class="item" value="NJ">New Jersey</div>
-<div class="item" value="NM">New Mexico</div>
-<div class="item" value="NY">New York</div>
-<div class="item" value="NC">North Carolina</div>
-<div class="item" value="ND">North Dakota</div>
-<div class="item" value="OH">Ohio</div>
-<div class="item" value="OK">Oklahoma</div>
-<div class="item" value="OR">Oregon</div>
-<div class="item" value="PA">Pennsylvania</div>
-<div class="item" value="RI">Rhode Island</div>
-<div class="item" value="SC">South Carolina</div>
-<div class="item" value="SD">South Dakota</div>
-<div class="item" value="TN">Tennessee</div>
-<div class="item" value="TX">Texas</div>
-<div class="item" value="UT">Utah</div>
-<div class="item" value="VT">Vermont</div>
-<div class="item" value="VA">Virginia</div>
-<div class="item" value="WA">Washington</div>
-<div class="item" value="WV">West Virginia</div>
-<div class="item" value="WI">Wisconsin</div>
-<div class="item" value="WY">Wyoming</div>
-</select> */}
 $(watchForm)
